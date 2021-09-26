41bc03cff1b115121b5bfbf533c57c45877214b1
richardx14 <22540891+richardx14@users.noreply.github.com>
2021-09-26 11:06:23 +0100

    working standalone calculate wip and flow times

--- src/calculateFlowTimes.py
+++ src/calculateFlowTimes.py
@@ -5,33 +5,6 @@ from datetime import date
 import statistics
 import utilityFunctions
 
-def validRow(row):
-            
-    if row[0] in utilityFunctions.ignoreList:
-        #print(f"{row[0]} in ignoreList")
-        return False
-            
-    if row[inProgressColumn] == "" and row[deployingColumn] == "" and row[deployedColumn] == "" and row[doneColumn] =="": 
-        #print(f"{row[0]} blank row skipped")
-        return False
-            
-        # skip if done ticket in previous month, this can happen if someone has updated commented on a done ticket
-        
-    if row[doneColumn] != "" and row[doneColumn][-5:-3] != currentMonth:
-        #print(f"{row[0]} ticket closed in previous month, row skipped")
-        return False
-    
-    return True
-
-def convertDateFomat(jiraDate):
-    year = int(jiraDate[0:4])
-    month = int(jiraDate[5:7])
-    day = int(jiraDate[-2:])
-    
-    formattedDate = date(year, month, day)
-    
-    return(formattedDate)
-
 def calculateFlowTime(startDate, endDate):
     
     fdate = date(int(startDate[0:4]),int(startDate[5:7]),int(startDate[-2:]))
@@ -58,14 +31,6 @@ flowTimes = []
 
 fileName = "/Users/richard.holloway/Development/hmrc/jira-to-analytics/dlk-output.csv"
 
-inProgressColumn = 5
-deployingColumn = 6
-deployedColumn = 7
-doneColumn = 8
-labelsColumn = 10
-createdColumn = 12
-statusColumn = 13
-
 skippedStories = []
 
 with open(fileName, newline='') as csvfile:
@@ -83,46 +48,46 @@ with open(fileName, newline='') as csvfile:
         if array[0] == "ID": # skip header row
             continue
 
-        if validRow(array) == False:
+        if utilityFunctions.validRow(array, currentMonth) == False:
             skippedStories.append(array[0])
             continue
         
-        if array[statusColumn] == "To Do" or array[statusColumn] == "Backlog":
+        if array[utilityFunctions.statusColumn] == "To Do" or array[utilityFunctions.statusColumn] == "Backlog":
             # story moved back so ignore for now.
             skippedStories.append[f"{array[0]} - story on backlog or Ready"]
             continue
         
-        if array[inProgressColumn] != "":
-            dateRangeForRow.append(array[inProgressColumn])
+        if array[utilityFunctions.inProgressColumn] != "":
+            dateRangeForRow.append(array[utilityFunctions.inProgressColumn])
             
-        if array[deployingColumn] != "":
-            dateRangeForRow.append(array[deployingColumn])
+        if array[utilityFunctions.deployingColumn] != "":
+            dateRangeForRow.append(array[utilityFunctions.deployingColumn])
             
-        if array[deployedColumn] != "":
-            dateRangeForRow.append(array[deployedColumn])
+        if array[utilityFunctions.deployedColumn] != "":
+            dateRangeForRow.append(array[utilityFunctions.deployedColumn])
             
-        if array[doneColumn] != "":
-            dateRangeForRow.append(array[doneColumn])
+        if array[utilityFunctions.doneColumn] != "":
+            dateRangeForRow.append(array[utilityFunctions.doneColumn])
         
         earliestDate = min(dateRangeForRow)
         latestDate = max(dateRangeForRow)
         
-        convertedEarliestDate = convertDateFomat(earliestDate)
-        convertedLatestDate = convertDateFomat(latestDate)
+        convertedEarliestDate = utilityFunctions.convertDateFomat(earliestDate)
+        convertedLatestDate = utilityFunctions.convertDateFomat(latestDate)
         
         delta = convertedLatestDate - convertedEarliestDate
         
-        if array[doneColumn] != "":
+        if array[utilityFunctions.doneColumn] != "":
             
             flowTime = delta.days
             
             flowTimes.append(flowTime)
         
-            print(f"{jiraId} earliest date {earliestDate}, latest date {latestDate}, flowTime = {flowTime}, labels = {array[labelsColumn]}")
+            print(f"{jiraId} earliest date {earliestDate}, latest date {latestDate}, flowTime = {flowTime}, labels = {array[utilityFunctions.labelsColumn]}")
         
         else:
             
-            print(f"{jiraId} earliest date {earliestDate}, latest date {latestDate}, labels = {array[labelsColumn]}")
+            print(f"{jiraId} earliest date {earliestDate}, latest date {latestDate}, labels = {array[utilityFunctions.labelsColumn]}")
         
 print()
 print(f"skippedStories {skippedStories}")
